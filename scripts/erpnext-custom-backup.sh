7436b668120f9903e81c9afa893f05f4d1d7179c
Kadir Yücel <lordkadir@gmail.com>
2025-05-12 11:54:44 +0200

    Backup improvements

--- scripts/erpnext-custom-backup.sh
+++ scripts/erpnext-custom-backup.sh
@@ -4,6 +4,6 @@
 source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
 # Execute the Docker command
-docker compose --project-name $ERPNEXT_PROJECT_NAME -f $ERPNEXT_YAML_FILE up backup
+docker compose --project-name $ERPNEXT_PROJECT_NAME -f $ERPNEXT_TARGET_YAML_FILE up backup
 
 echo "Backup executed."
